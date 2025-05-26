8dbd9d8a4ef841323e2a420b0da90f5eb8d54e6d
kpisti18 <k.istvanzoltan@gmail.com>
2025-05-26 11:30:01 +0200

    login.js

--- js/registration.js
+++ js/registration.js
@@ -10,4 +10,39 @@ async function register() {
 
     console.log(email, name, psw, psw2);
     
+    if (psw != psw2) {
+        alert("A két jelszó nem egyezik!");
+        return;
+    }
+
+    try {
+        const response = await fetch('http://127.0.0.1:3000/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ email, name, psw })
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        if (response.ok) {
+            window.location.href = '../html/login.html';
+        } else if (data.errors) {
+            let errorMessage = '';
+            data.errors.forEach(hiba => {
+                errorMessage += `${hiba.error}\n`;
+            });
+            alert(errorMessage);
+        } else if (data.error) {
+            alert(data.error);
+        } else {
+            alert('Ismeretlen hiba!');
+        }
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
